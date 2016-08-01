5980870ca70a3e675930c4e48b5a9f65439ba2f7
Adriana Galvez <vntgh@yahoo.com>
2016-08-01 13:30:31 -0600

    connected to google oauth

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var passport = require('passport');
+var bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 module.exports = router;
