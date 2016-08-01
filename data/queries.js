b955635acf9d849ca8eb4090ef21f7a5cca12616
Adriana Galvez <vntgh@yahoo.com>
2016-08-01 16:16:25 -0600

    login/logout/signup local and google oauth connected to db

---
+++ data/queries.js
@@ -0,0 +1,22 @@
+var knex = require('../db/knex');
+
+module.exports = {
+  createNewUser: function(form){
+    return knex.raw(`insert into users (name, email, password) values('${form.name}', '${form.email}', '${form.password}')`); //password hashing in route
+  },
+  createNewGoogleUser: function(gdata){
+    return knex.raw(`insert into users (name, email, "googleKey") values('${gdata.name}', '${gdata.email}', '${gdata.id}')`);
+  },
+  getUserData: function(id){
+    return knex.raw(`select * from users where id=${id}`);
+  },
+  getUserDataWithEmail: function(email){
+    return knex.raw(`select * from users where email='${email}'`); //for Google login and verification
+  },
+  connectGoogle: function(gdata){
+    return knex.raw(`update users set "googleKey"='${gdata.id}' where email='${gdata.email}'`);
+  },
+  getUserDataWithGKey: function(google_key){
+    return knex.raw(`select * from users where "googleKey"='${google_key}'`);
+  }
+};
