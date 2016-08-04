3366fc5e11787f4f099e9998a417c0df2f1e4d1d
Adriana Galvez <vntgh@yahoo.com>
2016-08-04 13:28:41 -0600

    if user doesn't have any trips display button to add trip

--- data/queries.js
+++ data/queries.js
@@ -18,5 +18,8 @@ module.exports = {
   },
   getUserDataWithGKey: function(google_key){
     return knex.raw(`select * from users where "googleKey"='${google_key}'`);
+  },
+  getLastTrip: function(user_id){
+    return knex.raw(`select activities.id as id, activities.name as name, trips.user_id as user_id, activities.address, activities.phone, activities.start_time, activities.end_time, days.date, trips.start_date, trips.end_date, trips.city from activities join days on activities.day_id = days.id join trips on trips.id = days.trip_id join users on users.id = trips.user_id where trips.id=(select MAX(id) from trips where trips.user_id=${user_id}) order by days.id`)
   }
 };
