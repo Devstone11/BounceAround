72f5d6ca5799a2dcb63e8de0b8a3e3831a37a4bb
Adriana Galvez <vntgh@yahoo.com>
2016-08-07 16:50:30 -0600

    padding

--- public/javascripts/getFancy.js
+++ public/javascripts/getFancy.js
@@ -1,4 +1,4 @@
-$(".getFancy:odd").css({"background-color": "#808080", "width": "100%", "border": "1px solid black"});
-$(".getFancy:even").css({"background-color": "#d3d3d3", "width": "100%", "border": "1px solid black"});
-$(".trippy:odd").css({"background-color": "#808080", "width": "100%", "border": "1px solid black"});
-$(".trippy:even").css({"background-color": "#d3d3d3", "width": "100%", "border": "1px solid black"});
+$(".getFancy:odd").css({"background-color": "#808080", "width": "100%", "border": "1px solid black", "padding": "2%"});
+$(".getFancy:even").css({"background-color": "#d3d3d3", "width": "100%", "border": "1px solid black", "padding": "2%"});
+$(".trippy:odd").css({"background-color": "#808080", "width": "100%", "border": "1px solid black", "padding": "2%"});
+$(".trippy:even").css({"background-color": "#d3d3d3", "width": "100%", "border": "1px solid black", "padding": "2%"});
